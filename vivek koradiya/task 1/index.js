a8d9aad9947cde65017b9e8b813a49fd6669631e
koradiyavivek <37997168+koradiyavivek@users.noreply.github.com>
2021-04-27 21:11:14 +0530

    Add files via upload

---
+++ vivek koradiya/task 1/index.js
@@ -0,0 +1,29 @@
+// part 1
+// https://www.indiatimes.com/news/
+
+// # The number of articles on the page
+
+
+// # The number of images on the page
+document.getElementsByTagName("img").length // I got 70
+
+
+// # The number of exit links on the page
+let exit_link_count = 0; 
+
+// getting all links from the site
+Array.from(document.getElementsByTagName("a")).forEach(elm => {
+    if(!elm.href) return;
+    
+    const link = (elm.href);
+
+    // checkint weather links is exit link or not
+    const origin = location.origin;
+    if (link.startsWith(origin)) {
+        // not an exit link
+    } else {
+        // exit link
+        exit_link_count++;
+    }
+})
+console.log(exit_link_count) // after running the code I got 95
---
+++ vivek koradiya/task 2/inex.js
@@ -0,0 +1,17 @@
+Array.from(document.getElementsByTagName("a")).forEach(elm => {
+    // if don't have href attribute then a tag is not a valid link
+    if(!elm.href) return;
+
+    elm.onclick = function() {
+        alert(elm.innerText);
+    }
+})
+
+Array.from(document.getElementsByTagName("a")).forEach(elm => {
+    // if don't have href attribute then a tag is not a valid link
+    if(!elm.href) return;
+
+    elm.onclick = function() {
+        window.open(elm.href,'popup','width=600,height=600')
+    }
+})
